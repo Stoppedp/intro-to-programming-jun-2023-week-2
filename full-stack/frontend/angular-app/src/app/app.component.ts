7e27ff9e8fd6f9262dd494e176124419e22726d4
Qing Liang <liangqian1994@gmail.com>
2023-06-26 16:57:12 -0400

    code update

--- full-stack/frontend/angular-app/src/app/app.component.ts
+++ full-stack/frontend/angular-app/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { BankAccountComponent } from './complonents/bank-account.component';
+import { BankAccountComponent } from './components/bank-account.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { HeadingComponent_1 as HeadingComponent } from "./complonents/heading/heading.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BankAccountComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule,
+    RouterOutlet,
+    BankAccountComponent,
+    HeadingComponent, NavigationComponent, HeadingComponent]
 })
 export class AppComponent {
-  title = 'angular-app';
+
 }
--- full-stack/frontend/angular-app/src/app/services/bonus-calculator.service.ts
+++ full-stack/frontend/angular-app/src/app/services/bonus-calculator.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: 'root' })
 export class BonusCalculator {
     calculateBonusForDepositOn(balance: number, amount: number): number {
         return balance >= 5000 ? amount * .10 : 0;
